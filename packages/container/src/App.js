06aac9e60e1582e90f718ed433588d2da8d4afa8
Rob Thorne <rob@torenware.com>
2020-12-24 13:48:11 -0800

    Namespaced production CSS for @material_ui

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -2,14 +2,24 @@ import React from 'react';
 import MarketingAppRef from './components/MarketingAppRef';
 import { BrowserRouter } from 'react-router-dom';
 import Header from './components/Header';
+import {
+    StylesProvider,
+    createGenerateClassName,
+} from '@material-ui/core/styles';
+
+const generateClassName = createGenerateClassName({
+    productionPrefix: 'co',
+});
 
 export default () => {
     return (
         <BrowserRouter>
-            <div>
-                <Header />
-                <MarketingAppRef />
-            </div>
+            <StylesProvider generateClassName={generateClassName}>
+                <div>
+                    <Header />
+                    <MarketingAppRef />
+                </div>
+            </StylesProvider>
         </BrowserRouter>
     );
 }
