003a56deb17b475b399601bc074f356c61baab7c
Rob Thorne <rob@torenware.com>
2020-12-23 22:03:06 -0800

    Start to add Header/CSS

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,6 +1,15 @@
 import React from 'react';
 import MarketingAppRef from './components/MarketingAppRef';
+import { BrowserRouter } from 'react-router-dom';
+import Header from './components/Header';
 
 export default () => {
-    return <MarketingAppRef />;
+    return (
+        <BrowserRouter>
+            <div>
+                <Header />
+                <MarketingAppRef />
+            </div>
+        </BrowserRouter>
+    );
 }
