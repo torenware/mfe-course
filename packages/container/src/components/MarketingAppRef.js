aca308f429ee1d9c2006af7e1fb96b8046ac983e
Rob Thorne <rob@torenware.com>
2020-12-24 18:16:28 -0800

    Added distributed navigation

--- packages/container/src/components/MarketingAppRef.js
+++ packages/container/src/components/MarketingAppRef.js
@@ -1,13 +1,28 @@
 import React, { useEffect, useRef } from 'react';
-import { mount } from 'marketing/MarketingApp'
+import { mount } from 'marketing/MarketingApp';
+// Hook to get our browser history.
+import { useHistory } from 'react-router-dom';
 
 
 export default () => {
     const ref = useRef(null);
+    const history = useHistory();
 
     useEffect(() => {
-        mount(ref.current);
-    });
+        const { onParentNavigate } = mount(ref.current, {
+            // history.listen will pass in a location obj. Pop
+            // pathname off of the object, and rename it.
+            onNavigate: ({ pathname: nextPathname }) => {
+                // Risk of an infinite loop. Make sure that path has
+                // indeed changed.
+                const { pathname } = history.location;
+                if (pathname !== nextPathname) {
+                    history.push(nextPathname);
+                }
+            },
+        });
+        history.listen(onParentNavigate);
+    }, []);
 
     return <div ref={ref}></div>
 };
--- packages/marketing/src/bootstrap.js
+++ packages/marketing/src/bootstrap.js
@@ -1,20 +1,44 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createMemoryHistory, createBrowserHistory } from 'history';
 import App from './App';
 
 // Mount function to start up the app
-const mount = (el) => {
-  ReactDOM.render(<App />, el);
+const mount = (el, { onNavigate, defaultHistory }) => {
+
+  // @see https://github.com/ReactTraining/history/blob/master/docs/api-reference.md
+  // If we are running in isolation, we use a default browser history, otherwise,
+  // maintain a memory history for our MFE bookkeeping.
+  const history = defaultHistory || createMemoryHistory();
+
+  // Update the container's notion of history. Note that when this particular
+  // MFE is called in isolation, we will not be passed anything from a container.
+  if (onNavigate) {
+    history.listen(onNavigate);
+  }
+
+  ReactDOM.render(<App history={history} />, el);
+
+  // We need to pass back some way of updating this MFE to the container.
+  return {
+    onParentNavigate({ pathname: nextPathname }) {
+      const { pathname } = history.location;
+      if (pathname !== nextPathname) {
+        history.push(nextPathname);
+      }
+    }
+  };
 };
 
 
 
 if (process.env.NODE_ENV === 'development') {
-    const el = document.querySelector('#_marketing-dev-root');
-  
-    if (el) {
-      mount(el);
-    }
+  const el = document.querySelector('#_marketing-dev-root');
+
+  if (el) {
+    // Make sure we get onNavigate as null when called in dev.
+    mount(el, { defaultHistory: createBrowserHistory() });
   }
-  
-  export { mount };
+}
+
+export { mount };
